89b79895ff1cc74432c530088af44cc4496b2042
sergey2342 <sergey_2342@tut.by>
2020-02-15 16:59:29 +0300

    Update server.php

--- src/server.php
+++ src/server.php
@@ -1,18 +1,3 @@
 <?php 
 
-server {
-    listen       80;
-    server_name  localhost;
-
-    location / {
-        root   html;
-        index  index.html index.htm;
-    }
-
-    error_page  404     /404.html;
-    error_page  403     /403.html;
-
-    error_page  405     =200 $uri;
-
-    # ...
-}
+error_page 405 =200 @405;
